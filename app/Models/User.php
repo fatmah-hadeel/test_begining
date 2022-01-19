89d15d1bbbe6c1000b534c491e2328fb70eca4fb
fatmah-hadeel <fatimah1089@gmail.com>
2022-01-19 10:19:38 +0300

    upload-image6

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,7 +58,7 @@ class User extends Authenticatable
 
         $filename = $image->getClientOriginalName();
         (new self())->deleteOldImage();
-        $image->storeAs('images',$filename,'do_spaces', 'public');
+        $image->storeAs('images',$filename,'do_spaces')->putFile('public');
         auth()->user()->update(['avatar' => $filename]);
     }
 
